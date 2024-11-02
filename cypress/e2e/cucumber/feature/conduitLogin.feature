4b19cbb1a92bf8fec44500cda78db986d52b55f5
Jesse <ya710728@gmail.com>
2024-11-02 17:51:51 +0800

    add fixture file for test

--- cypress/e2e/cucumber/feature/conduitLogin.feature
+++ cypress/e2e/cucumber/feature/conduitLogin.feature
@@ -3,6 +3,8 @@ Feature: Conduit Login Functionality
   Scenario: Login and Logout with Valid Credentials
     Given User is on the login page
     When User login with valid credentials
+      | email               | password |
+      | jcjchuhu2@gmail.com | abcd1234 |
     And User click on the settings button
     And User click on the logout button
     Then User should be routed back to login page
--- cypress/e2e/cucumber/stepDefinitions/conduitLogin/conduitLogin_stepdef.js
+++ cypress/e2e/cucumber/stepDefinitions/conduitLogin/conduitLogin_stepdef.js
@@ -1,25 +1,29 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
-Given("User is on the login page", () => {
+beforeEach(function () {
+  cy.fixture("conduitLoginData").as("data");
+});
+
+Given("User is on the login page", function () {
   cy.visit("https://conduit.bondaracademy.com/");
   cy.contains("Sign in").click();
 });
 
-When("User login with valid credentials", () => {
-  cy.get('input[placeholder="Email"]').type("jcjchuhu2@gmail.com");
-  cy.get('input[placeholder="Password"]').type("abcd1234");
+When("User login with valid credentials", function (dataTable) {
+  cy.get('input[placeholder="Email"]').type(dataTable.rawTable[1][0]);
+  cy.get('input[placeholder="Password"]').type(dataTable.rawTable[1][1]);
   cy.contains("button", "Sign in").click();
 });
 
-When("User click on the settings button", () => {
+When("User click on the settings button", function () {
   cy.get('a[href="/settings"]').click();
 });
 
-When("User click on the logout button", () => {
+When("User click on the logout button", function () {
   cy.contains("button", "Or click here to logout.").click();
 });
 
-Then("User should be routed back to login page", () => {
+Then("User should be routed back to login page", function () {
   // cy.title() 是用來檢查 html 的 title 標籤，後面接斷言 should()
   cy.title().should("eq", "Conduit | Practice Test Automation");
 });
